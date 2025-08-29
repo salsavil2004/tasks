f0c4eb89225a38dfe4bd4e1f0bd7002417563459
Salsavil Chowdhury <salsavilchowdhury@wifi-roaming-128-4-176-134.wifi.udel.edu>
2025-08-29 11:08:11 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App(): React.JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
+                Hi, my name is Salsavil Chowdhury!
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
