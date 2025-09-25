2eadaeab2d4611c8ee2b668ff7e03428e27987c7
salsavil2004 <salsavil@udel.edu>
2025-09-24 20:45:58 -0400

    Update question.ts

--- src/interfaces/question.ts
+++ src/interfaces/question.ts
@@ -1,22 +1,18 @@
-/** QuestionType influences how a question is asked and what kinds of answers are possible */
+/**
+ * A union type for the two kinds of questions in this project
+ */
 export type QuestionType = "multiple_choice_question" | "short_answer_question";
 
-/** A representation of a Question in a quizzing application */
+/**
+ * A representation of a Question in a quizzing application
+ */
 export interface Question {
-    /** A unique identifier for the question */
-    id: number;
-    /** The human-friendly title of the question */
-    name: string;
-    /** The instructions and content of the Question */
-    body: string;
-    /** The kind of Question; influences how the user answers and what options are displayed */
-    type: QuestionType;
-    /** The possible answers for a Question (for Multiple Choice questions) */
-    options: string[];
-    /** The actually correct answer expected */
-    expected: string;
-    /** How many points this question is worth, roughly indicating its importance and difficulty */
-    points: number;
-    /** Whether or not this question is ready to display to students */
-    published: boolean;
+    id: number;              // Unique identifier
+    name: string;            // Name/title of the question
+    type: QuestionType;      // The type of the question
+    body: string;            // The actual question content
+    expected: string;        // The correct answer
+    options: string[];       // Only used if multiple_choice_question
+    points: number;          // Points assigned to the question
+    published: boolean;      // Whether the question is visible/published
 }
