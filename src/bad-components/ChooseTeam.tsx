93d8e064a0de8ddb3a9da85b7e10e049f3362e1f
salsavil2004 <salsavil@udel.edu>
2025-10-02 14:49:53 -0400

    Update ChooseTeam.tsx

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -16,12 +16,12 @@ export function ChooseTeam(): React.JSX.Element {
 
     function chooseMember(newMember: string) {
         if (!team.includes(newMember)) {
-            setTeam([...team, newMember]); 
+            setTeam([...team, newMember]);
         }
     }
 
     function clearTeam() {
-        setTeam([]); 
+        setTeam([]);
     }
 
     return (
@@ -29,26 +29,28 @@ export function ChooseTeam(): React.JSX.Element {
             <h3>Choose Team</h3>
             <Row>
                 <Col>
-                    {allOptions.map((option: string) => (
-                        <div key={option} style={{ marginBottom: "4px" }}>
-                            Add{" "}
-                            <Button
-                                onClick={() => chooseMember(option)}
-                                size="sm"
-                            >
-                                {option}
-                            </Button>
-                        </div>
-                    ))}
+                    {allOptions.map((option: string) => {
+                        return (
+                            <div key={option} style={{ marginBottom: "4px" }}>
+                                Add{" "}
+                                <Button
+                                    onClick={() => { chooseMember(option); }}
+                                    size="sm"
+                                >
+                                    {option}
+                                </Button>
+                            </div>
+                        );
+                    })}
                 </Col>
                 <Col>
                     <strong>Team:</strong>
                     <ul>
-                        {team.map((member: string) => (
-                            <li key={member}>{member}</li>
-                        ))}
+                        {team.map((member: string) => {
+                            return <li key={member}>{member}</li>;
+                        })}
                     </ul>
-                    <Button onClick={clearTeam}>Clear Team</Button>
+                    <Button onClick={() => { clearTeam(); }}>Clear Team</Button>
                 </Col>
             </Row>
         </div>
