1e936e87ffad015697935baf285112c0758ebe52
salsavil <salsavil@udel.edu>
2025-10-04 13:28:37 -0400

    fixed bad components

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,36 +4,6 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
-function ChangeColor({
-    colorIndex,
-    setColorIndex
-}: {
-    colorIndex: number;
-    setColorIndex: (newIndex: number) => void;
-}): React.JSX.Element {
-    return (
-        <Button onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}>
-            Next Color
-        </Button>
-    );
-}
-
-function ColorPreview({ color }: { color: string }): React.JSX.Element {
-    return (
-        <div
-            data-testid="colored-box"
-            style={{
-                width: "50px",
-                height: "50px",
-                backgroundColor: color,
-                display: "inline-block",
-                verticalAlign: "bottom",
-                marginLeft: "5px",
-            }}
-        ></div>
-    );
-}
-
 export function ColoredBox(): React.JSX.Element {
     const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
 
@@ -42,11 +12,22 @@ export function ColoredBox(): React.JSX.Element {
             <h3>Colored Box</h3>
             <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
-                <ChangeColor
-                    colorIndex={colorIndex}
-                    setColorIndex={setColorIndex}
-                ></ChangeColor>
-                <ColorPreview color={COLORS[colorIndex]}></ColorPreview>
+                <Button
+                    onClick={() => { setColorIndex((1 + colorIndex) % COLORS.length); }}
+                >
+                    Next Color
+                </Button>
+                <div
+                    data-testid="colored-box"
+                    style={{
+                        width: "50px",
+                        height: "50px",
+                        backgroundColor: COLORS[colorIndex],
+                        display: "inline-block",
+                        verticalAlign: "bottom",
+                        marginLeft: "5px",
+                    }}
+                ></div>
             </div>
         </div>
     );
--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -3,32 +3,15 @@ import { Button } from "react-bootstrap";
 
 function ShoveBoxButton({
     position,
-    setPosition
+    setPosition,
 }: {
     position: number;
     setPosition: (newPosition: number) => void;
 }) {
     return (
-        <Button onClick={() => setPosition(position + 4)}>
+        <Button onClick={() => { setPosition(position + 4); }}>
             Shove the Box
         </Button>
-    );
-}
-
-function MoveableBox({ position }: { position: number }): React.JSX.Element {
-    return (
-        <div
-            data-testid="moveable-box"
-            style={{
-                width: "50px",
-                height: "50px",
-                backgroundColor: "lightblue",
-                border: "1px solid blue",
-                display: "inline-block",
-                verticalAlign: "bottom",
-                marginLeft: position + "px",
-            }}
-        ></div>
     );
 }
 
@@ -38,13 +21,21 @@ export function ShoveBox(): React.JSX.Element {
     return (
         <div>
             <h3>Shove Box</h3>
-            <span>The box is at: {position}px</span>
+            <span>The box is at: {position}</span>
             <div>
-                <ShoveBoxButton
-                    position={position}
-                    setPosition={setPosition}
-                ></ShoveBoxButton>
-                <MoveableBox position={position}></MoveableBox>
+                <ShoveBoxButton position={position} setPosition={setPosition} />
+                <div
+                    data-testid="moveable-box"
+                    style={{
+                        width: "50px",
+                        height: "50px",
+                        backgroundColor: "lightblue",
+                        border: "1px solid blue",
+                        display: "inline-block",
+                        verticalAlign: "bottom",
+                        marginLeft: position + "px",
+                    }}
+                ></div>
             </div>
         </div>
     );
