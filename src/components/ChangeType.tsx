b1eae914939a29c6e8e9b4648534783594959522
salsavil <salsavil@udel.edu>
2026-02-16 12:11:37 -0500

    2nd assignment

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -6,10 +6,11 @@ export function ChangeType(): React.JSX.Element {
     const [type, setType] = useState<QuestionType>("short_answer_question");
 
     const toggleType = (): void => {
-        setType((prevType) =>
-            prevType === "short_answer_question"
-                ? "multiple_choice_question"
-                : "short_answer_question"
+        setType(
+            (prevType: QuestionType): QuestionType =>
+                prevType === "short_answer_question" ?
+                    "multiple_choice_question"
+                :   "short_answer_question",
         );
     };
 
@@ -17,9 +18,9 @@ export function ChangeType(): React.JSX.Element {
         <div>
             <Button onClick={toggleType}>Change Type</Button>
             <span style={{ marginLeft: "10px" }}>
-                {type === "short_answer_question"
-                    ? "Short Answer"
-                    : "Multiple Choice"}
+                {type === "short_answer_question" ?
+                    "Short Answer"
+                :   "Multiple Choice"}
             </span>
         </div>
     );
