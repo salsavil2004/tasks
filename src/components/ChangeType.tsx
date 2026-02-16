8bcfbb41c220356b27bc1eeada7358cb9c705b02
salsavil <salsavil@udel.edu>
2026-02-15 21:11:08 -0500

    solved-html-css assignment

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -5,18 +5,18 @@ import { QuestionType } from "../interfaces/question";
 export function ChangeType(): React.JSX.Element {
     const [type, setType] = useState<QuestionType>("short_answer_question");
 
-    function toggleType(): void {
-        setType(
-            type === "short_answer_question"
+    const toggleType = (): void => {
+        setType((prevType) =>
+            prevType === "short_answer_question"
                 ? "multiple_choice_question"
                 : "short_answer_question"
         );
-    }
+    };
 
     return (
         <div>
             <Button onClick={toggleType}>Change Type</Button>
-            <span>
+            <span style={{ marginLeft: "10px" }}>
                 {type === "short_answer_question"
                     ? "Short Answer"
                     : "Multiple Choice"}
