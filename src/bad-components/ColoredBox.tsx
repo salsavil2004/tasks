595ea0ce67158f14b0117801558786369ef8db58
salsavil2004 <salsavil@udel.edu>
2026-03-09 20:38:13 -0400

    Update ColoredBox.tsx

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -1,10 +1,36 @@
-
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
+function ChangeColor({
+    colorIndex,
+    setColorIndex,
+}: {
+    colorIndex: number;
+    setColorIndex: (index: number) => void;
+}): React.JSX.Element {
+    const handleClick = () => setColorIndex((colorIndex + 1) % COLORS.length);
+
+    return <Button onClick={handleClick}>Next Color</Button>;
+}
+
+function ColorPreview({ colorIndex }: { colorIndex: number }): React.JSX.Element {
+    return (
+        <div
+            data-testid="colored-box"
+            style={{
+                width: "50px",
+                height: "50px",
+                backgroundColor: COLORS[colorIndex],
+                display: "inline-block",
+                verticalAlign: "bottom",
+                marginLeft: "5px",
+            }}
+        ></div>
+    );
+}
 export function ColoredBox(): React.JSX.Element {
     const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
 
@@ -13,22 +39,8 @@ export function ColoredBox(): React.JSX.Element {
             <h3>Colored Box</h3>
             <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
-                <Button
-                    onClick={() => { setColorIndex((1 + colorIndex) % COLORS.length); }}
-                >
-                    Next Color
-                </Button>
-                <div
-                    data-testid="colored-box"
-                    style={{
-                        width: "50px",
-                        height: "50px",
-                        backgroundColor: COLORS[colorIndex],
-                        display: "inline-block",
-                        verticalAlign: "bottom",
-                        marginLeft: "5px",
-                    }}
-                ></div>
+                <ChangeColor colorIndex={colorIndex} setColorIndex={setColorIndex} />
+                <ColorPreview colorIndex={colorIndex} />
             </div>
         </div>
     );
